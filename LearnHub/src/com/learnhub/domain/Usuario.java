6e0bec499435399cd28c53779499d85798c9c2a0
Tevezx <teveztwitch@gmail.com>
2026-04-03 00:16:59 -0300

    feat: adicionando atributos ao usuario

---
+++ LearnHub/src/com/learnhub/domain/Usuario.java
@@ -0,0 +1,14 @@
+package com.learnhub.domain;
+
+import java.util.Date;
+
+public class Usuario {
+    private String id;
+    private String nome;
+    private String email;
+    private Date dataCadastro;
+    // private Status status;
+
+
+
+}
