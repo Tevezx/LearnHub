5535e3e69bcc54bd2baf3c3f54a06fc429ebcd75
Tevezx <teveztwitch@gmail.com>
2026-04-07 14:31:11 -0300

    feat: adicionando funcionalidade de criacao de aluno

--- LearnHub/src/com/learnhub/domain/usuario/Aluno.java
+++ LearnHub/src/com/learnhub/domain/usuario/Aluno.java
@@ -1,13 +1,35 @@
 package com.learnhub.domain.usuario;
 
+import com.learnhub.domain.curso.Curso;
+import com.learnhub.service.AlunoService;
+
 import java.util.Date;
 import java.util.List;
 
 public class Aluno extends Usuario{
+    private AlunoService alunoService;
+    private Curso curso;
 
     public Aluno(String id, String nome, String email, Date dataCadastro, Status status) {
         super(id, nome, email, dataCadastro, status);
     }
+
+    public Aluno criarAluno(String id, String nome, String email, Date dataCadastro, Status status){
+        return alunoService.criarAluno(id, nome, email, dataCadastro, status);
+    }
+
+
+
+     @Override
+    public String toString() {
+         return "Aluno{" +
+                 "id='" + getId() + '\'' +
+                 ", nome='" + getNome() + '\'' +
+                 ", email='" + getEmail() + '\'' +
+                 ", dataCadastro=" + getDataCadastro() +
+                 ", status=" + getStatus() +
+                 '}';
+     }
 
 
 }
---
+++ LearnHub/src/com/learnhub/service/AlunoService.java
@@ -0,0 +1,13 @@
+package com.learnhub.service;
+
+import com.learnhub.domain.curso.Curso;
+import com.learnhub.domain.usuario.Aluno;
+import com.learnhub.domain.usuario.Status;
+
+public class AlunoService {
+    public Aluno criarAluno(String id, String nome, String email, java.util.Date dataCadastro, Status status){
+        return new Aluno(id, nome, email, dataCadastro, status);
+    }
+
+
+}
